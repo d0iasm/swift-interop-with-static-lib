9f0253e9292b729d2825d05cff7e48753246664d
Asami Doi <asamidoi@chromium.org>
2025-11-07 14:51:15 +0100

    Initial Commit

---
+++ swift-interop-with-static-lib/swift_interop_with_static_libApp.swift
@@ -0,0 +1,17 @@
+//
+//  swift_interop_with_static_libApp.swift
+//  swift-interop-with-static-lib
+//
+//  Created by Asami Doi on 07/11/2025.
+//
+
+import SwiftUI
+
+@main
+struct swift_interop_with_static_libApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
